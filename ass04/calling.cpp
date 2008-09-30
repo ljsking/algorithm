49bcb3bf651e7bb5d1ed301fae5befeeae7a46e8
ljsking <ljsking@gmail.com>
2008-09-30 00:48:23 -0700

    got 100 points for sample data.

--- ass04/calling.cpp
+++ ass04/calling.cpp
@@ -6,6 +6,7 @@
 #include <cassert>
 #include <iostream>
 #include <fstream>
+#include <set>
 using namespace std;
 
 list<int> adjList;
@@ -19,11 +20,28 @@ public:
 	int to;
 };
 
-
-int _tmain(int argc, _TCHAR* argv[])
+class Student
 {
-	int size = 0;
+public:
+	Student(int ID):id(ID){adjList = new set<int>();group=new int(-1);}
+	int id;
+	int *group;
+	set<int>* adjList;
+	bool operator<(const Student& other)const
+	{
+		return id<other.id;
+	}
+	bool operator==(const Student& other)const
+	{
+		return id==other.id;
+	}
+};
+
+
+int main(int argc, char* argv[])
+{
 	vector<Edge> edges;
+	set<Student> students;
 	ifstream inf("calling.inp");
 	int edgesize;
 	inf>>edgesize;
@@ -32,63 +50,66 @@ int _tmain(int argc, _TCHAR* argv[])
 		int from, to;
 		inf>>from>>to;
 		edges.push_back(Edge(from, to));
+		students.insert(Student(from));
+		students.insert(Student(to));
+		
 	}
 	inf.close();
-	for(vector<Edge>::iterator iter=edges.begin(); iter!=edges.end(); iter++)
-		size = max(iter->to, size);
-	vector< list<int>* > adjLists(size);
-	for(int i = 0; i<size; i++)
-		adjLists[i] = new list<int>;
-	
-	vector<int> rz(size);
-	for(int i = 0; i<size; i++)
-		rz[i] = -1;
-
-	for(vector<Edge>::iterator iter=edges.begin(); iter!=edges.end(); iter++)
-	{
-		adjLists[iter->from-1]->push_back(iter->to-1);
-		adjLists[iter->to-1]->push_back(iter->from-1);
-	}
-	int maxgroup = 0;
-	for(int i = 0; i<size; i++)
-	{
-		list<int> *adjList = adjLists[i];
-		int group = rz[i] == -1?maxgroup++:rz[i];
 		
+	for(vector<Edge>::iterator iter=edges.begin(); iter!=edges.end(); iter++)
+	{
+		students.find(Student(iter->from))->adjList->insert(iter->to);
+		students.find(Student(iter->to))->adjList->insert(iter->from);
+	}
+	
+	vector<int> groups;
+	for(set<Student>::iterator iter = students.begin(); iter!=students.end(); iter++)
+	{
 		vector<int> stack;
-		for(list<int>::iterator iter = adjList->begin(); iter!=adjList->end(); iter++)
+		int group;
+		if(*(iter->group) == -1)
+		{
+			group = groups.size();
+			*(iter->group) = group;
+			groups.push_back(1);
+		}
+		else
+			group = *(iter->group);
+		
+		set<int> *adjList = iter->adjList;
+		for(set<int>::iterator iter = adjList->begin(); iter!=adjList->end(); iter++)
 			stack.push_back(*iter);
 
 		while(stack.size())
 		{
-			int val = stack.back();
+			set<Student>::iterator s = students.find(Student(stack.back()));
 			stack.pop_back();
-			if(rz[val] == -1)
+			if(*(s->group) == -1)
 			{
-				rz[val] = group;
-				adjList = adjLists[val];
-				for(list<int>::iterator iter = adjList->begin(); iter!=adjList->end(); iter++)
+				*(s->group) = group;
+				groups[group]++;
+				for(set<int>::iterator iter = s->adjList->begin(); iter!=s->adjList->end(); iter++)
 					stack.push_back(*iter);
 			}
 		}
 	}
-	assert(3==maxgroup);
-	vector<int> groups(maxgroup);
-	vector<int> leaders(maxgroup);
-	int maxNumberOfPeople = -1;
-	for(int i=0; i<size; i++){
-		groups[rz[i]]++;
-		maxNumberOfPeople = max(maxNumberOfPeople, groups[rz[i]]);
-		leaders[rz[i]]=i+1;
-	}
-	int finalLeader = -1;
-	for(int i=0; i<maxgroup;i++)
+	int maxNumber = *max_element(groups.begin(), groups.end());
+	
+	vector<int> rzGroups;
+	for(vector<int>::iterator iter = groups.begin(); iter!=groups.end(); iter++)
+		if(maxNumber==*iter)
+			rzGroups.push_back(iter-groups.begin());
+	
+	for(set<Student>::reverse_iterator iter = students.rbegin(); iter!=students.rend(); iter++)
 	{
-		if(maxNumberOfPeople == groups[i])
-			finalLeader = max(finalLeader, leaders[i]);
+		for(vector<int>::iterator g = rzGroups.begin();g!=rzGroups.end();g++)
+			if(*g == *(iter->group))
+			{
+				ofstream of("calling.out");
+				of<<iter->id;
+				of.close();
+				return 0;
+			}
 	}
-	ofstream of("calling.out");
-	of<<finalLeader;
-	of.close();
-	return 0;
+	return -1;
 }
